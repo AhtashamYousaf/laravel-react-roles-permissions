d4bf9f13f11029294ba46b48b91786458fa4465a
Ahtasham <sheikhahtasham15@gmail.com>
2025-05-22 19:58:32 +0500

    update: make the user chart fully dynamic

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Inertia\Inertia;
+use Carbon\Carbon;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DashboardController extends Controller
 {
@@ -15,11 +18,23 @@ class DashboardController extends Controller
     {
         
         $users = User::all();
-       
-        return Inertia::render('dashboard', [
+
+        $chartData = collect(range(0, 89))->map(function ($i) {
+        $date = Carbon::now()->subDays(89 - $i)->format('Y-m-d');
+            return [
+                'date' => $date,
+                'mobile' => rand(50, 200),
+                'desktop' => rand(100, 300),
+            ];
+        })->values()->all();
+
+        return Inertia::render('dashboard/index', [
             'users' => $users,
+            'roles' => Role::all(),
+            'permissions' => Permission::all(),
             'mustVerifyEmail' => false,
             'status' => session('status'),
+            'chartData' => $chartData,
         ]);
     }
 }
--- resources/js/pages/dashboard/index.tsx
+++ resources/js/pages/dashboard/index.tsx
@@ -2,17 +2,10 @@ import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
-import {  TrendingUpIcon } from "lucide-react"
-import { Badge } from "@/components/ui/badge"
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { ChartAreaInteractive } from '@/pages/chart-area-interactive';
 import { type User } from '@/types';
+import StatCard from './components/card';
+import { TrendingUpIcon, ShieldQuestion, BookKey } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -21,54 +14,65 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+type ChartData = {
+  date: string
+  desktop: number
+  mobile: number
+}
 interface Props {
     users: User[];
+    roles: { id: number; name: string }[];
+    permissions: { id: number; name: string }[];
     status: string;
     mustVerifyEmail: boolean;
+    chartData: ChartData[];
 }
 
-export default function Dashboard({ users }: Props) {
+export default function Dashboard({ users, roles, permissions, chartData }: Props) {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 <div className="grid auto-rows-min gap-3 md:grid-cols-4">
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <Card className="absolute inset-0 flex flex-col justify-between">
-                            <CardHeader className="relative">
-                            <CardDescription>Total Users</CardDescription>
-                            <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
-                                {users.length}
-                            </CardTitle>
-                            <div className="absolute right-4 top-4">
-                                <Badge variant="outline" className="flex gap-1 rounded-lg text-xs">
-                                <TrendingUpIcon className="size-3" />
-                                +12.5%
-                                </Badge>
-                            </div>
-                            </CardHeader>
-                            <CardFooter className="flex-col items-start gap-1 text-sm p-4 pt-0">
-                            <div className="line-clamp-1 flex gap-2 font-medium">
-                                Trending up this month <TrendingUpIcon className="size-4" />
-                            </div>
-                            <div className="text-muted-foreground">
-                                Visitors for the last 6 months
-                            </div>
-                            </CardFooter>
-                        </Card>
+                        <StatCard 
+                            description="Total Users"
+                            cardTitle={users.length}
+                            badgeIcon={<TrendingUpIcon className="size-3" />} 
+                            badge="+12.5%" 
+                            footerIconTitle="Trending up this month"
+                            footerIcon={<TrendingUpIcon className="size-4" />} 
+                            footerDescription="Visitors for the last 6 months" 
+                        />
                     </div>
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                        <StatCard 
+                            description="Total Roles"
+                            cardTitle={roles.length}
+                            badgeIcon={<BookKey className="size-3" />} 
+                            badge="" 
+                            footerIconTitle=""
+                            footerIcon={<BookKey className="size-4" />} 
+                            footerDescription="" 
+                        />
                     </div>
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                        <StatCard 
+                            description="Total Permissions"
+                            cardTitle={permissions.length}
+                            badgeIcon={<ShieldQuestion className="size-3" />} 
+                            badge="" 
+                            footerIconTitle=""
+                            footerIcon={<ShieldQuestion className="size-4" />} 
+                            footerDescription="" 
+                        />
                     </div>
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
                 </div>
                  <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
-                    <ChartAreaInteractive />
+                    <ChartAreaInteractive chartData={chartData} />
                 </div>
                 <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
                     <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
