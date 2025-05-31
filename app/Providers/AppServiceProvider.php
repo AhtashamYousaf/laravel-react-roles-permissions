1a450c8df2c1541df1a7aea03b092b3f9feef2f6
ahtasham <sheikhahtasham15@gmail.com>
2025-05-31 14:41:21 +0500

    fix: added check in app serviceprovider to ensure database connection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,11 +24,15 @@ class AppServiceProvider extends ServiceProvider
     {
         
         // Check if settings table schema is present.
-        if (Schema::hasTable('settings')) {
-            $settings = Setting::pluck('option_value', 'option_name')->toArray();
-            foreach ($settings as $key => $value) {
-                config(['settings.' . $key => $value]);
+        try {
+            if (Schema::hasTable('settings')) {
+                $settings = Setting::pluck('option_value', 'option_name')->toArray();
+                foreach ($settings as $key => $value) {
+                    config(['settings.' . $key => $value]);
+                }
             }
+        } catch (\Exception $e) {
+            Log::warning('Skipping settings config load: ' . $e->getMessage());
         }
     }
 }
